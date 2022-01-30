08fd0fd9f1ec2f588e3120d43896cd14d8836fa4
Harsh Rohila <rohilaharsh@gmail.com>
2022-01-30 14:45:05 +0530

    added scss helpers

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,5 +1,5 @@
 import { Component, Host, h } from '@stencil/core';
-import { faBars, faCog, faBraille } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faCog, faBraille, faSearch, faSlidersH } from '@fortawesome/free-solid-svg-icons';
 import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
 @Component({
   tag: 'app-header',
@@ -11,18 +11,30 @@ export class AppHeader {
     return (
       <Host>
         <header>
-          <x-icon icon={faBars}></x-icon>
-          <img
-            class="gb_sc"
-            src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_1x_r2.png"
-            srcset="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_2x_r2.png 2x ,https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_1x_r2.png 1x"
-            alt=""
-            aria-hidden="true"
-          ></img>
-          <h1>Stencil App Starter</h1>
-          <x-icon icon={faQuestionCircle}></x-icon>
-          <x-icon icon={faCog}></x-icon>
-          <x-icon icon={faBraille}></x-icon>
+          <div class="left">
+            <div class="brand">
+              <x-icon class="bars" icon={faBars}></x-icon>
+              <img
+                class="gb_sc"
+                src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_1x_r2.png"
+                srcset="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_2x_r2.png 2x ,https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_1x_r2.png 1x"
+                alt=""
+                aria-hidden="true"
+              ></img>
+            </div>
+
+            <div class="search">
+              <x-icon icon={faSearch}></x-icon>
+              <input type="search" placeholder="Search mail" />
+              <x-icon icon={faSlidersH}></x-icon>
+            </div>
+          </div>
+
+          <div class="right">
+            <x-icon icon={faQuestionCircle}></x-icon>
+            <x-icon icon={faCog}></x-icon>
+            <x-icon icon={faBraille}></x-icon>
+          </div>
         </header>
       </Host>
     );
