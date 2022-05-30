0618602ab3765bd1b71a73f48061ac8d44e43f71
Harsh Rohila <rohilaharsh@gmail.com>
2022-05-30 08:45:05 +0530

    added spa support

--- apps/gmail-clone-app/stencil.config.ts
+++ apps/gmail-clone-app/stencil.config.ts
@@ -1,9 +1,9 @@
-import { Config } from '@stencil/core';
+import { Config } from '@stencil/core'
 
-import { sass } from '@stencil/sass';
+import { sass } from '@stencil/sass'
 // https://stenciljs.com/docs/config
-const dev: boolean = process.argv && process.argv.indexOf('--dev') > -1;
-const apiEnv: string = dev ? 'dev' : 'prod';
+const dev: boolean = process.argv && process.argv.indexOf('--dev') > -1
+const apiEnv: string = dev ? 'dev' : 'prod'
 export const config: Config = {
   globalStyle: 'src/global/app.scss',
   globalScript: 'src/global/app.ts',
@@ -14,18 +14,19 @@ export const config: Config = {
       // comment the following line to disable service workers in production
       serviceWorker: null,
       baseUrl: getBaseUrl(),
-    },
+      copy: [{ src: '404.html' }]
+    }
   ],
   env: {
-    apiEnv,
+    apiEnv
   },
-  plugins: [sass()],
-};
+  plugins: [sass()]
+}
 
 function getBaseUrl() {
   if (apiEnv === 'prod') {
-    return 'https://harshrohila.github.io';
+    return 'https://harshrohila.github.io'
   }
 
-  return 'https://.local/';
+  return 'https://.local/'
 }
