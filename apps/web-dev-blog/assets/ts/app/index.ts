d4cafd9b04cdb9936f0fdf53a0b2f3103d68da07
Harsh Rohila <rohilaharsh@gmail.com>
2022-07-20 21:35:10 +0530

    added vue js

--- apps/web-dev-blog/assets/ts/app/index.ts
+++ apps/web-dev-blog/assets/ts/app/index.ts
@@ -1,1 +1,16 @@
-console.log('Running TS')
+const cdn = 'https://cdn.jsdelivr.net/npm/vue@3/dist/vue.esm-browser.prod.js'
+
+async function main() {
+  const Vue = await import(cdn)
+
+  var app = Vue.createApp({
+    delimiters: ['[[', ']]'],
+    data() {
+      return {
+        message: 'hi'
+      }
+    }
+  }).mount('body')
+}
+
+main()
