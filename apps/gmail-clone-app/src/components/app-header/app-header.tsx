9d43d9639ccbd836361bf792fbaf65680e5d6f17
Harsh Rohila <harsh.rohila@mrisoftware.com>
2022-09-03 21:47:16 +0530

    2: added hover helper

--- apps/gmail-clone-app/src/components/app-header/app-header.tsx
+++ apps/gmail-clone-app/src/components/app-header/app-header.tsx
@@ -1,6 +1,13 @@
 /* eslint-disable react/jsx-no-bind */
 import { Component, Host, h } from '@stencil/core'
-import { faBars, faCog, faBraille, faSearch, faSlidersH } from '@fortawesome/free-solid-svg-icons'
+import {
+  faBars,
+  faCog,
+  faBraille,
+  faSearch,
+  faSlidersH,
+  faTrash
+} from '@fortawesome/free-solid-svg-icons'
 import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons'
 import { SideBar } from '../../states/sideBar'
 import { EmailSelection } from '../../states/emailSelection'
@@ -15,40 +22,48 @@ export class AppHeader {
   }
 
   render() {
-    return (
-      <Host>
-        <header>
-          {!this.isSelectionMode && (
-            <div class="left">
-              <div class="brand">
-                <MenuToggler />
-                <img
-                  class="gb_sc"
-                  src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_1x_r2.png"
-                  srcset="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_2x_r2.png 2x ,https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_1x_r2.png 1x"
-                  alt=""
-                  aria-hidden="true"
-                ></img>
-              </div>
-
-              <div class="search">
-                <MenuToggler />
-                <x-icon icon={faSearch}></x-icon>
-                <input type="search" placeholder="Search mail" />
-                <x-icon class="settings-icon" icon={faSlidersH}></x-icon>
-              </div>
-            </div>
-          )}
-
-          <div class="right">
-            <x-icon icon={faQuestionCircle}></x-icon>
-            <x-icon icon={faCog}></x-icon>
-            <x-icon icon={faBraille}></x-icon>
-          </div>
-        </header>
-      </Host>
-    )
+    return <Host>{this.isSelectionMode ? <SelectionModeHeader /> : <Header />}</Host>
   }
+}
+
+function SelectionModeHeader() {
+  return (
+    <header>
+      <icon-button class="delete-button" icon={faTrash}></icon-button>
+    </header>
+  )
+}
+
+function Header() {
+  return (
+    <header>
+      <div class="left">
+        <div class="brand">
+          <MenuToggler />
+          <img
+            class="gb_sc"
+            src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_1x_r2.png"
+            srcset="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_2x_r2.png 2x ,https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_1x_r2.png 1x"
+            alt=""
+            aria-hidden="true"
+          ></img>
+        </div>
+
+        <div class="search">
+          <MenuToggler />
+          <x-icon icon={faSearch}></x-icon>
+          <input type="search" placeholder="Search mail" />
+          <x-icon class="settings-icon" icon={faSlidersH}></x-icon>
+        </div>
+      </div>
+
+      <div class="right">
+        <x-icon icon={faQuestionCircle}></x-icon>
+        <x-icon icon={faCog}></x-icon>
+        <x-icon icon={faBraille}></x-icon>
+      </div>
+    </header>
+  )
 }
 
 function MenuToggler() {
