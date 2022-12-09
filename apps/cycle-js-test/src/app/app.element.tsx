9e3651280e15f82bd4c068efd470625cc538a8f2
Harsh Rohila <rohilaharsh@gmail.com>
2022-12-09 08:35:14 +0530

    using components with cycle js

--- apps/cycle-js-test/src/app/app.element.tsx
+++ apps/cycle-js-test/src/app/app.element.tsx
@@ -1,32 +1,61 @@
 import './app.element.css'
-import { makeDOMDriver } from '@cycle/dom'
+import { div, input, makeDOMDriver, p } from '@cycle/dom'
 import { run } from '@cycle/run'
-import Snabbdom from 'snabbdom-pragma'
+import isolate from '@cycle/isolate'
+import xs from 'xstream'
 
 export class AppElement extends HTMLElement {
   public static observedAttributes = []
 
   connectedCallback() {
     this.innerHTML = `
-    <div id="app">Hello</div>
+    <div id="app"></div>
     `
-    console.log(Snabbdom)
     run(main, drivers)
   }
 }
 
 function main(sources) {
+  const coolInputSources = { DOM: sources.DOM }
+  const coolInput = isolate(CoolInput)(coolInputSources)
+
+  const myCoolVDom$ = coolInput.DOM
+
+  const isChecked$ = sources.DOM.select('input')
+    .events('click')
+    .map(ev => ev.target.checked)
+    .startWith(false)
+
+  const vdom$ = xs.combine(myCoolVDom$, isChecked$).map(([myCoolVDom, isChecked]) =>
+    div([
+      myCoolVDom,
+      // prettier-ignore
+      div([
+          input('.check', { attrs: { type: 'checkbox' } }), 
+          p(isChecked ? 'ON' : 'off')
+        ])
+    ])
+  )
+
   const sinks = {
-    DOM: sources.DOM.select('input')
-      .events('click')
-      .map(ev => ev.target.checked)
-      .startWith(false)
-      .map(toggled => (
-        <div>
-          <input type="checkbox" /> Toggle me
-          <p>{toggled ? 'ON' : 'off'}</p>
-        </div>
-      ))
+    DOM: vdom$
+  }
+  return sinks
+}
+
+function CoolInput(sources) {
+  const sinks = {
+    DOM: sources.DOM.select('.harsh')
+      .events('input')
+      .map(ev => ev.target.value)
+      .startWith('')
+      .map(text =>
+        // prettier-ignore
+        div([
+          input('.harsh', {attrs: {type: 'text'}}),
+          p(text)
+        ])
+      )
   }
   return sinks
 }
